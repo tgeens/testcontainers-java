6c32a68ccd806362e702ffd4ac3cf25f1de7bf31
Toon Geens <toon.geens@xenit.eu>
2023-10-24 15:03:51 +0200

    Use single lower version 1.17

--- modules/k3s/src/test/java/org/testcontainers/k3s/OfficialClientK3sContainerTest.java
+++ modules/k3s/src/test/java/org/testcontainers/k3s/OfficialClientK3sContainerTest.java
@@ -19,13 +19,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class OfficialClientK3sContainerTest {
 
     @Test
-    public void shouldStartAndHaveListableNode() throws IOException, ApiException {
-        runK3s(DockerImageName.parse("rancher/k3s:v1.28.2-k3s1"));
-    }
-
-    @Test
     public void shouldStartAndHaveListableNodeUsingLowerVersion() throws IOException, ApiException {
-        runK3s(DockerImageName.parse("rancher/k3s:v1.20.15-k3s1"));
+        runK3s(DockerImageName.parse("rancher/k3s:v1.17.17-k3s1"));
     }
 
     private void runK3s(DockerImageName k3sDockerImage) throws IOException, ApiException {
